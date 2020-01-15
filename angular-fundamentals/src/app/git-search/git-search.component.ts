b46f799df8fc13d0fdbb9dba94526d85b9890280
@lebarquero <lebarquero@gmail.com>
2020-01-15 16:59:01 -0600

    angular-fundamentals progress

---
+++ angular-fundamentals/src/app/git-search/git-search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { GitSearchService } from '../git-search.service';
+import { GitSearch } from '../git-search';
+
+@Component({
+  selector: 'app-git-search',
+  templateUrl: './git-search.component.html',
+  styleUrls: ['./git-search.component.css']
+})
+export class GitSearchComponent implements OnInit {
+  searchResults: GitSearch;
+  
+  constructor(private GitSearchService: GitSearchService) { }
+
+  ngOnInit() {
+  }
+
+}
